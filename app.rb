66b145804910d031b30a6ab375732cef1a258b42
David Mwangi <david.mathenge98@gmail.com>
2019-05-28 01:04:37 +0300

    feat(auth): check whether user already exists

--- app.rb
+++ app.rb
@@ -18,11 +18,17 @@ end
 
 post '/api/v1/auth/signup' do
   payload = JSON.parse(request.body.read)
-  user = User.new(payload)
-
-  if user.save
-    user.to_json
+  existing_user = User.where(email: payload['email']).first
+  
+  if existing_user
+    halt 409, { 'status': 'error', 'message': 'User with the email already exists' }.to_json
   else
-    halt 400, user.errors.full_messages.to_json
+    user = User.new(payload)
+
+    if user.save
+      user.to_json
+    else
+      halt 400, user.errors.full_messages.to_json
+    end
   end
 end
