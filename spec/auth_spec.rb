de4f835689103deb533e549b67c4fe8cd9724664
David Mwangi <david.mathenge98@gmail.com>
2019-06-11 00:15:22 +0300

    feat(tests) add signup and signin page render tests

--- spec/auth_spec.rb
+++ spec/auth_spec.rb
@@ -2,14 +2,21 @@ require_relative '../spec/spec_helper.rb'
 require_relative '../app/app.rb'
 
 describe 'Auth controller' do
-  data = {
-    firstName: "David",
-    lastName: "Mwangi",
-    email: "dmwangihh@gmail.com",
-    password: "DMwangi2019"
-  }
+  it 'should render signup page' do
+    visit '/signup'
+    expect(current_path).to eq('/signup')
+  end
 
   it 'should successfully register a user' do
+    visit '/signup'
+  end
+
+  it 'should render signin page' do
+    visit '/signin'
+    expect(current_path).to eq('/signin')
+  end
+
+  it 'should successfully login a user' do
     visit '/signup'
   end
   
