b549183bb4d83f70493a3d2352fe88ff1c396b51
David Mwangi <david.mathenge98@gmail.com>
2019-06-10 18:16:02 +0300

    feat(auth): add user signup

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,7 +1,25 @@
+require_relative '../models/user'
+
 class UserController < MicroLearningApp
-  post '/signup' do
+  get '/signup' do
+    erb :signup
   end
 
-  post '/signin' do
+  get '/signin' do
+    erb :signin
+  end
+
+  post '/auth/signup' do
+    @user = User.new
+    @user.first_name = params[:firstName]
+    @user.last_name = params[:secondName]
+    @user.email = params[:email]
+    @user.password = params[:password]
+
+    @user.save
+
+    # existing_user = User.where(email: params['email']).first
+    
+    redirect '/signup'
   end
 end
